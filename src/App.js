b0cfe602af7419148f2449c50bcb86dcf5edc885
Mettihew <90737870+mettihew@users.noreply.github.com>
2025-01-22 09:58:42 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     const [modal, setModal] = useState(false);
 
   return (
-    <BrowserRouter basename="/" >
+    <BrowserRouter basename="/eesy" >
       <div className={modal ? "blackModal" : ""} />
       <Header onBlur={ev => setModal(ev)} />
       <Routes>
